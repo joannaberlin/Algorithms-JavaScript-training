c8069611594583691ed87bb3a7d69b65b504ea1f
joannakot <joanna.kot.ok@gmail.com>
2021-12-28 17:27:59 +0100

    replace method

---
+++ kata-12/index.js
@@ -0,0 +1,39 @@
+/* 
+DNA to RNA Conversion
+
+Deoxyribonucleic acid, DNA is the primary information storage 
+molecule in biological systems. It is composed of four nucleic 
+acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and 
+Thymine ('T').
+
+Ribonucleic acid, RNA, is the primary messenger molecule in cells. 
+RNA differs slightly from DNA its chemical structure and contains 
+no Thymine. In RNA Thymine is replaced by another nucleic acid 
+Uracil ('U').
+
+Create a function which translates a given DNA string into RNA.
+
+For example:
+
+"GCAT"  =>  "GCAU"
+The input string can be of arbitrary length - in particular, it may 
+be empty. All input is guaranteed to be valid, i.e. each input string
+will only ever consist of 'G', 'C', 'A' and/or 'T'.
+*/
+
+function DNAtoRNA(dna) {
+	for (let char of dna) {
+		if (char === "T") {
+			dna.replace("T", "U");
+			dna = dna.replace("T", "U");
+		}
+	}
+	return dna;
+}
+
+DNAtoRNA("TTTT");
+//"UUUU"
+DNAtoRNA("GCAT");
+//"GCAU"
+DNAtoRNA("GACCGCCGCC");
+//"GACCGCCGCC"
